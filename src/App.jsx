7a326c16b82abf5b33c1e11526f6317c7bc8b5c6
lilif <liliflilif21@gmail.com>
2025-02-12 11:36:00 +0530

    this circle

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Circletask from './Components/Circles';
+import Trail from './Components/Trail';
 
 const App = () => {
   return (
 <>
 <Circletask/>
-
 </>
   )
 }
